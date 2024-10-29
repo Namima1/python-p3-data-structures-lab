eb390ed69503c6a3ebd050febe2761a1c1fb07ec
Namima Islam <namimaislam308@gmail.com>
2024-10-29 00:45:23 -0400

    complete labs

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -15,24 +15,57 @@ spicy_foods = [
         "heat_level": 6,
     },
 ]
-
+#1
 def get_names(spicy_foods):
-    pass
+    names = []
+    for food in spicy_foods:
+        names.append(food["name"])
+    return names
 
+#2
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest = []
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spiciest.append(food)
+    return spiciest
 
+
+#3
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        heat_level_emojis = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {heat_level_emojis}")
 
+#4
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
 
+#5
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in get_spiciest_foods(spicy_foods):
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{name} ({cuisine}) | Heat Level: {heat_level}")
 
+
+#6
 def get_average_heat_level(spicy_foods):
-    pass
+    total = 0
+    for food in spicy_foods:
+        total += food["heat_level"]
+    average = total / len(spicy_foods)
+    return int(average)
 
+#7
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
